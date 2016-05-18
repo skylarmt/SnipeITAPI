83cf691a961fdaadc74798ec6a12a449e144f2d9
Skylar <admin@netsyms.com>
2016-05-18 14:48:51 -0600

    Add support for status labels for assets.

---
+++ getstatus.php
@@ -0,0 +1,27 @@
+<?php
+
+require 'required.php';
+//require 'dieifnotloggedin.php';
+
+$from = $_GET['from'];
+require 'readfrom.php';
+
+$id = $_GET['id'];
+if (is_empty($id)) {
+    sendError('Missing item ID!');
+}
+$status;
+if ($from == 'assets') {
+    $status = $database->select($from, 'status_id', ['id' => $id])[0];
+} else {
+    sendError("Command only valid for assets.");
+}
+
+if ($status == null) {
+    $status = 0;
+}
+
+$list = $database->select('status_labels', ['id', 'name', 'notes']);
+//array_unshift($list, ['id' => "0", name => "Other"]);
+
+die(json_encode(['status' => 'OK', 'itemstatus' => $status, 'list' => $list]));
